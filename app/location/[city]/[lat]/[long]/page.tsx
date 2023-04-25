d17414f51ae6149e7279576e3a9a83aec943fb5a
Esau Ndhlovu <ndhloves@zra.org.zm>
2023-04-25 12:49:09 +0200

    First Commit

---
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { getClient } from '../../../../../apollo-client';
+import fetchWeatherQuery from '../../../../../graphql/queries/fetchWeatherQueries';
+
+type Props = {
+    params:{
+        city:string;
+        lat:string;
+        long:string;
+    }
+}
+
+const WeatherPage = async ({params:{city, lat, long}}:Props) => {
+  const client = getClient();
+  const { data } = await client.query({
+    query:fetchWeatherQuery,
+    variables:{
+      current_weather:"true",
+      latitude:lat,
+      longitude:long,
+      timezone:"GMT"
+    }
+  });
+  const results:Root = data.myQuery;
+  console.log(results);
+
+  return (
+    <div>Welcome to the weather page: {city} {lat} {long}</div>
+  )
+}
+
+export default WeatherPage
---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React from 'react'
+import "./globals.css";
+import { Card, Divider, Subtitle, Text } from '@tremor/react'
+import CityPicker from './components/CityPicker';
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-[#394F68] to-[#18387E] 
+    p-10 flex flex-col justify-center items-center'>
+        <Card>
+            <Text className='text-6xl font-bold text-center mb-10'>Weather AI</Text>
+             {/* <h1>Let's build a GPT-4 App</h1> */}
+             <Subtitle className='text-xl text-center'>
+                Powered by OpenAI, Next.js 13, Tailwind CSS, Tremor 2.0 + more!
+             </Subtitle>
+             <Divider className='my-10'/>
+             <Card className='bg-gradient-to-br from-[#394F68] to-[#18387E]'>
+                <CityPicker />
+             </Card>
+        </Card>
+    </div>
+   
+  )
+}
+
+export default Home
